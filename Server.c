7f9b79e3d46473c704254d7d4f7f31d580666102
HashedDan <dmangum@hashedhealth.com>
2017-10-02 22:59:15 -0500

    added server file

---
+++ Server.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/sockets.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include "Practical.h"
+
+static const int MAXPENDING = 5;
+
+int main(int argc, char * argv[]) {
+	
+}
